c30383cb42fa24c9eee5b6bfdcb35766ab2cf455
James Holland <jamoiholland@gmail.com>
2022-03-16 18:13:46 +0000

    updates

--- aws.tf
+++ aws.tf
@@ -4,7 +4,6 @@ resource "aws_s3_bucket" "data" {
   # bucket does not have access logs
   # bucket does not have versioning
   bucket                    = "data"
-  acl                       = "public-read"
   force_destroy             = true
 }
 
@@ -13,7 +12,6 @@ resource "aws_rds_cluster" "app1-rds-cluster" {
     # rds has no backup policy
     # rds does not have deletion protection
     cluster_identifier      = "app1-rds-cluster"
-    allocated_storage       = 10
     backup_retention_period = 0
 }
 
@@ -22,9 +20,8 @@ resource "aws_instance" "ec2_vm" {
   # ec2 has no monitoring enabled
   # ec2 has Instance Metadata Service Version 1 enabled
   # ec2 is not EBS optimized
-  ami                       = "${var.ami}"
-  instance_type             = "${var.instance_type}"
-  subnet_id                 = "${var.subnet_id}"
-  ebs_optimized = true
+  ami                       = var.ami
+  instance_type             = var.instance_type
+  subnet_id                 = var.subnet_id
 }
 
